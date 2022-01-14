7a80999dfe1d2ecd897b32424f1814dfa0663ab7
bo <>
2022-01-14 16:49:00 +0800

    覆盖路由，手动指定转发地址

--- gateway/src/main/java/com/bobo/demo/gateway/filter/SessionAuthFilter.java
+++ gateway/src/main/java/com/bobo/demo/gateway/filter/SessionAuthFilter.java
@@ -3,6 +3,8 @@ package com.bobo.demo.gateway.filter;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.Route;
@@ -10,12 +12,11 @@ import org.springframework.core.Ordered;
 import org.springframework.http.HttpCookie;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
+import javax.annotation.Resource;
+import java.util.List;
 
-import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
 
 /**
@@ -26,6 +27,9 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.G
 public class SessionAuthFilter implements GlobalFilter, Ordered {
 
     private final RedissonClient redissonClient;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     public SessionAuthFilter(RedissonClient redissonClient) {
         this.redissonClient = redissonClient;
@@ -42,21 +46,35 @@ public class SessionAuthFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
+        if (route == null) {
+            return chain.filter(exchange);
+        }
         System.out.println(route);
         RMap<Object, Object> session_token = redissonClient.getMap("session_id_user");
-
         exchange.getRequest().getCookies().toSingleValueMap().forEach((k, v) -> System.out.println(k + "-->" + v));
         //TODO 从注册中心获取服务真实列表
         HttpCookie bobo_session_id = exchange.getRequest().getCookies().getFirst("bobo_session_id");
         String cookie = bobo_session_id == null ? "" : bobo_session_id.getValue();
-        Object o = session_token.get(cookie); // 假定这个对象是实际要转发的服务ip
 
-        URI requestUrl = UriComponentsBuilder.fromUri(exchange.getRequest().getURI()).uri(route.getUri()).build().toUri();
-        System.out.println(requestUrl);
-        System.out.println(URI.create("http://127.0.0.1:10003" + exchange.getRequest().getURI().getPath()));
-        exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, URI.create("http://127.0.0.1:10003" + exchange.getRequest().getURI().getPath()));
+        System.out.println(route.getUri().getAuthority());
+        List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances("demo-auth");
+        serviceInstanceList.forEach(serviceInstance -> {
+            //获取host
+            serviceInstance.getHost();
+            //获取端口号
+            serviceInstance.getPort();
+            serviceInstance.getServiceId();
+        });
 
-        return chain.filter(exchange).then();
+        Object o = session_token.get(cookie); // 通过用户判断需要转发的地址
+
+
+        //重定义路由
+        Route newRoute = Route.async().asyncPredicate(route.getPredicate()).filters(route.getFilters()).id(route.getId()).order(route.getOrder()).
+                uri("http://192.168.50.116:10011").build();
+        exchange.getAttributes().put(GATEWAY_ROUTE_ATTR, newRoute);
+
+        return chain.filter(exchange);
 
     }
 
